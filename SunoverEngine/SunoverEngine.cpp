5f3806ef8d3ed272370586e1cbee09e151129f55
Miron Quabitum <miron.quabitum@gmail.com>
2026-08-19 19:30:45 +0200

    Empty Engine

---
+++ SunoverEngine/SunoverEngine.cpp
@@ -0,0 +1,12 @@
+﻿// SunoverEngine.cpp: определяет точку входа для приложения.
+//
+
+#include "SunoverEngine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
